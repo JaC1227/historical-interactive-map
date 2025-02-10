74cd9601e452afba2b107352acdc9f16bb2c3700
JaC1227 <156141013+JaC1227@users.noreply.github.com>
2025-02-10 00:11:53 -0500

    vite-plugin-gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { ghPages } from "vite-plugin-gh-pages"; 
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  sourcemap: true,
-  plugins: [react()],
-  base: "/historical-interactive-map/"
-
-})
+  base: "/historical-interactive-map/", 
+  plugins: [ghPages()],   
+  build: {
+    sourcemap: true,
+  },
+});
