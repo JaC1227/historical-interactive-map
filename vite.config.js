168d29128140e3b7a1490d1260847d0b07180b5b
JaC1227 <156141013+JaC1227@users.noreply.github.com>
2025-02-10 00:28:25 -0500

    geojson moved to dist

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import { ghPages } from "vite-plugin-gh-pages"; 
+import VitePluginCopy from 'vite-plugin-copy';
 
 export default defineConfig({
   base: "/historical-interactive-map/", 
-  plugins: [ghPages()],   
+  plugins: [
+    ghPages(),
+    VitePluginCopy({
+      targets: [
+        {
+          src: 'borders_117AD.geojson', // Update with the actual path to your GeoJSON file
+          dest: 'dist', // Copy it to the root of dist
+        },
+      ],
+    }),
+  ],
   build: {
     sourcemap: true,
   },
